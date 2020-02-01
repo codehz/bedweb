7687c384ebb061629331dc4d680087871ac6d5bb
CodeHz <CodeHz@outlook.com>
2020-02-01 19:13:48 +0800

    fix terminal

--- src/xterm.ts
+++ src/xterm.ts
@@ -23,11 +23,13 @@ export default {
         fontSize: 16,
         allowTransparency: true,
         cursorStyle: "block",
+        logLevel: "error",
         theme: {
           foreground: "black",
           background: "rgba(255, 255, 255, 0.8)",
           cursor: "black",
           cursorAccent: "black",
+          selection: "rgba(0, 0, 0, 0.8)",
           black: "#000000",
           brightBlack: "#191919",
           red: "#683c39",
@@ -49,6 +51,7 @@ export default {
       term.open(host.render() as any);
       host.fit = () => {
         const [hw, hh] = [host.offsetWidth, host.offsetHeight];
+        if (hw == 0 || hh == 0) return;
         const [cw, ch] = getCellSize(term);
         const [tw, th] = [hw / cw | 0, hh / ch | 0];
         term.resize(tw, th);
